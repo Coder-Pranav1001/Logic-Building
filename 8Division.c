32b633748923d26173b6aa79d33d7b86af208dcc
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-08-10 00:36:07 +0530

    Accept the number from user and check whether it is divisible by 5 or not

---
+++ 8Division.c
@@ -0,0 +1,41 @@
+/*
+	Description :- Accept the number from user and check whether it is divisible by 5 or not
+	Author :- Pranav R Sonawane
+*/
+
+#include<stdio.h>
+
+int Division(int iNo)
+{
+	int iAns = 0;
+	iAns = iNo % 5;
+	
+	if(iAns == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+  
+int main()
+{
+	int iValue = 0;
+	int iRet = 0;
+	
+	printf("Enter the number : \n");
+	scanf("%d",&iValue);
+	
+	iRet = Division(iValue);
+	if(iRet == 0)
+	{
+		printf("%d is not divisible by 5\n",iValue);
+	}
+	else
+	{
+		printf("%d is divisible by 5\n",iValue);
+	}
+	return 0;
+}
