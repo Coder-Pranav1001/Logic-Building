bf6f03fe8eb9c122fd880193cafd5ff0cb7d6518
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-08-10 00:34:19 +0530

    Multiplication of two Numbers

---
+++ 6Multiplication.c
@@ -0,0 +1,27 @@
+/*
+	Description :- Multiplication of two Numbers
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+
+int main()
+{
+	int iNo1 = 0;
+	int iNo2 = 0;
+	int iMult = 0;
+	
+	printf("Enter First Number : \n");
+	scanf("%d",&iNo1);
+	
+	printf("Enter Second Number : \n");
+	scanf("%d",&iNo2);
+	
+	printf("First Number is : %d\n",iNo1);
+	printf("Second Number is : %d\n",iNo2);
+	
+	iMult = iNo1 * iNo2;
+	
+	printf("Multiplication is : %d\n",iMult);
+	
+	return 0;
+}
