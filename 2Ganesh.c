cda139061dda73dab831f78e8f2ced556c45b146
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-08-10 00:32:25 +0530

    Print Jay Ganesh on consol using Functional Approach

---
+++ 2Ganesh.c
@@ -0,0 +1,18 @@
+/*
+	Description :- Print Jay Ganesh on consol using Functional Approach
+	Author :- Pranav R Sonawane
+*/
+
+#include<stdio.h>
+
+void Display()
+{
+	printf("Jay Ganesh...");
+}
+
+int main()
+{
+	Display();
+	
+	return 0;
+}
