978752356a26881261c2cc5966c88310ffda6a7b
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-08-10 23:51:15 +0530

    Display Five times Jay Ganesh without using loop

---
+++ 15Display.c
@@ -0,0 +1,27 @@
+/*
+	Description :- Display Five times Jay Ganesh without using loop
+	Author :- Pranav R Sonawane
+*/
+/*
+Return_Datatype Function_Name(Parameters_list)
+{
+	// Function Body -> Sequence / Selection / Iteration / Combination of all
+}
+*/
+#include<stdio.h>
+
+void Display()
+{
+	printf("Jay Ganesh...\n");
+	printf("Jay Ganesh...\n");
+	printf("Jay Ganesh...\n");
+	printf("Jay Ganesh...\n");
+	printf("Jay Ganesh...\n");
+}
+
+int main()
+{
+	Display();
+	
+	return 0;
+}
