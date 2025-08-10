cbe8dc6ee1e022ddfd20385f408e8ebc29bf3c6d
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-08-10 23:52:32 +0530

    Display Five times Jay Ganesh using for loop

---
+++ 16Display.c
@@ -0,0 +1,21 @@
+/*
+	Description :- Display Five times Jay Ganesh using for loop
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+
+void Display()
+{
+	int iCnt = 0;
+	for(iCnt = 1; iCnt <= 5; iCnt++)
+	{
+		printf("Jay Ganesh...\n");
+	}
+}
+
+int main()
+{
+	Display();
+	
+	return 0;
+}
