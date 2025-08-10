a0d8bbb0714d9aa5a4b70a79f2505b8b313a5787
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-08-10 23:53:27 +0530

    Demonstration of iteration using while loop

---
+++ 18Display.c
@@ -0,0 +1,24 @@
+/*
+	Description :- Demonstration of iteration using while loop
+	Author :- Pranav R Sonawane
+*/  
+#include<stdio.h>
+
+void Display()
+{
+	int iCnt = 0;
+	
+	iCnt = 1;
+	while(iCnt <= 5)
+	{
+		printf("Jay Ganesh...\n");
+		iCnt++;
+	}
+}
+
+int main()
+{
+	Display();
+	 
+	return 0;
+}
