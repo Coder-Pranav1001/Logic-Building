1f6ab32b01b6c4dd15abbfabc1f026ec8fee55a0
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-08-13 23:30:24 +0530

    Summation of 1 to 5 numbers

---
+++ 27Summation.c
@@ -0,0 +1,28 @@
+/*
+	Description :- Summation of 1 to 5 numbers
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+int Summation()
+{
+	int iSum = 0;
+	
+	iSum = iSum + 1;
+	iSum = iSum + 2;
+	iSum = iSum + 3;
+	iSum = iSum + 4;
+	iSum = iSum + 5;
+	
+	return iSum;
+}
+
+int main()
+{
+	int iRet = 0;
+	
+	iRet = Summation();
+	
+	printf("Summation is : %d\n",iRet);
+	
+	return 0;
+}
