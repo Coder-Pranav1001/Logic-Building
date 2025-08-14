e3a62a543f945dc5bb476556243a91a159928e3a
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-08-14 22:06:15 +0530

    Display Factors from given number

---
+++ 31Factors.c
@@ -0,0 +1,31 @@
+/*
+	Description :- Display Factors from given number
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+// Time Complexity : O(N)
+void DisplayFactors(int iNo)
+{
+	int iCnt = 0;
+	
+	printf("Factors are : \n");
+	for(iCnt = 1; iCnt < iNo; iCnt++)
+	{
+		if((iNo % iCnt) == 0)
+		{
+			printf("%d\n",iCnt);
+		}
+	}
+}
+
+int main()
+{
+	int iValue = 0;
+	
+	printf("Enter the Number\n");
+	scanf("%d",&iValue);
+	
+	DisplayFactors(iValue);
+	
+	return 0;
+}
