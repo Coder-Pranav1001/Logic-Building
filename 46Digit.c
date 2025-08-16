652da9a4e5e51515de900da1ad776985b351048b
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-08-16 22:58:36 +0530

    Accept number from user and count digit in that number

---
+++ 46Digit.c
@@ -0,0 +1,33 @@
+/*
+	Description :- Accept number from user and count digit in that number 
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+
+int CountDigit(int iNo)
+{
+	int iDigit = 0;
+	int iCnt = 0;
+	
+	while(iNo != 0)
+	{
+		iDigit = iNo % 10;
+		iNo = iNo / 10;
+		iCnt++;
+	}
+	return iCnt;
+}
+
+int main()
+{
+	int iValue = 0;
+	int iRet = 0;
+	
+	printf("Enter the number : \n");
+	scanf("%d",&iValue);
+	
+	iRet = CountDigit(iValue);
+	printf("Number of Digits are : %d\n",iRet);
+	
+	return 0;
+}
