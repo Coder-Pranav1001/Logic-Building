dbfeb9ab6c21e93878606dbc661db99f9aa2d9db
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-08-16 23:00:58 +0530

    Updater

---
+++ 49Updater.c
@@ -0,0 +1,38 @@
+/*
+	Description :- Updater
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+
+int CountDigit(int iNo)
+{
+	int iDigit = 0;
+	int iCnt = 0;
+	
+	if(iNo < 0) // Updater
+	{
+		iNo = -iNo; 
+	}
+	
+	while(iNo > 0) // its works on positive number
+	{
+		iDigit = iNo % 10;
+		iNo = iNo / 10;
+		iCnt++;
+	}
+	return iCnt;
+}
+
+int main()
+{
+	int iValue = 0;
+	int iRet = 0;
+	
+	printf("Enter the number : \n");
+	scanf("%d",&iValue);
+	
+	iRet = CountDigit(iValue);
+	printf("Number of Digits are : %d\n",iRet);
+	
+	return 0;
+}
