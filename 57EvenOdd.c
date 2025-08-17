ba5bd5607d5d2fcc45e68d1a688854e1c35058af
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-08-17 22:19:10 +0530

    Accept the number from user and count even and odd digit from that number and Display it

---
+++ 57EvenOdd.c
@@ -0,0 +1,45 @@
+/*
+	Description :- Accept the number from user and count even and odd digit from that number and Display it
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+
+void DisplayEvenOddDigits(int iNo)
+{
+	int iDigit = 0;
+	int iEvenCnt = 0;
+	int iOddCnt = 0;
+	
+	if(iNo == 0) 
+	{
+		iEvenCnt++;
+	}
+	
+	while(iNo != 0) 
+	{
+		iDigit = iNo % 10;
+		if((iDigit % 2) == 0)
+		{
+			iEvenCnt++;
+		}
+		else
+		{
+			iOddCnt++;
+		}
+		iNo = iNo / 10;
+	}
+	printf("Number of Even Digits are : %d\n",iEvenCnt);
+	printf("Number of Odd Digits are : %d\n",iOddCnt);
+}
+
+int main()
+{
+	int iValue = 0;
+	
+	printf("Enter the number : \n");
+	scanf("%d",&iValue);
+	
+	DisplayEvenOddDigits(iValue);
+	
+	return 0;
+}
