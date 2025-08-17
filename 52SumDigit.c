a4f7fd49967ac571664f65a02fd58cce2ce3b7c7
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-08-17 22:16:00 +0530

    Accept the number from user and return the summation of Digits

---
+++ 52SumDigit.c
@@ -0,0 +1,34 @@
+/*
+	Description :- Accept the number from user and return the summation of Digits
+	Author :- Pranav R Sonawane
+*/
+
+#include<stdio.h>
+
+int SumDigits(int iNo)
+{
+	int iDigit = 0;
+	int iSum = 0;
+	
+	while(iNo != 0) 
+	{
+		iDigit = iNo % 10;
+		iNo = iNo / 10;
+		iSum = iSum + iDigit;
+	}
+	return iSum;
+}
+
+int main()
+{
+	int iValue = 0;
+	int iRet = 0;
+	
+	printf("Enter the number : \n");
+	scanf("%d",&iValue);
+	
+	iRet = SumDigits(iValue);
+	printf("Summation of Digits are : %d\n",iRet);
+	
+	return 0;
+}
