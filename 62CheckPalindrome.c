5af766f8a61f9b10a242ae578bc35b3143ec231a
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-08-18 20:28:41 +0530

    Accept the number from user and Check the number is palindrome or not using for loop

---
+++ 62CheckPalindrome.c
@@ -0,0 +1,50 @@
+/*
+	Description :- Accept the number from user and Check the number is palindrome or not using for loop
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckPalindrome(int iNo)
+{
+	int iDigit = 0;
+	int iRev = 0;
+	int iTemp = iNo;
+	
+	for( ; iNo != 0; )
+	{
+		iDigit = iNo % 10;
+		iRev = (iRev * 10) + iDigit;
+		iNo = iNo / 10;
+	}
+	
+	if(iRev == iTemp)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}	
+}
+
+int main()
+{
+	int iValue = 0;
+	bool bRet = false;
+	
+	printf("Please enter the number : \n");
+	scanf("%d",&iValue);
+	
+	bRet = CheckPalindrome(iValue);
+	
+	if(bRet == true)
+	{
+		printf("%d is a palindrome number\n",iValue);
+	}
+	else
+	{
+		printf("%d is not a palindrome number\n",iValue);
+	}
+	return 0;
+}
