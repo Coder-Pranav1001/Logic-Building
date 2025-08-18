f93165de978a03ac485496684d9f68a542f483cc
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-08-18 20:29:34 +0530

    Accept the number from user and find Maximum digit from that number

---
+++ 64MaxDigit.c
@@ -0,0 +1,41 @@
+/*
+	Description :- Accept the number from user and find Maximum digit from that number
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+int MaxDigit(int iNo)
+{
+	int iDigit = 0;
+	int iMax = 0;
+	
+	if(iNo < 0)
+	{
+		iNo = -iNo;
+	}
+    
+	while(iNo != 0)
+	{ 
+		iDigit = iNo % 10;
+		if (iDigit > iMax)
+		{
+			iMax = iDigit;
+		}
+		iNo = iNo / 10;
+	}
+	return iMax;
+}
+
+int main()
+{
+	int iValue = 0;
+	int iRet = 0;
+	
+	printf("Enter the number : \n");
+	scanf("%d",&iValue);
+	
+	iRet = MaxDigit(iValue);
+	
+	printf("Largest digit is : %d\n",iRet);
+	
+	return 0;
+}
