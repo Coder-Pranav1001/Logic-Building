81247423258cbbb76259a13cf916bf84b9dca280
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-08-18 20:35:04 +0530

    Accept N numbers from user Display all Numbers on Console using loop

---
+++ 69Display.c
@@ -0,0 +1,27 @@
+/*
+	Description :- Accept N numbers from user Display all Numbers on Cansole using loop
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+
+int main()
+{
+	int Arr[5];
+	int iCnt = 0;
+	
+	printf("Enter the elements : \n");
+	
+	for(iCnt = 0; iCnt < 5; iCnt++)
+	{
+		scanf("%d",&Arr[iCnt]);
+	}
+
+	printf("Elements from array are : \n");
+	
+	for(iCnt = 0; iCnt < 5; iCnt++)
+	{
+		printf("%d\n",Arr[iCnt]);
+	}
+	
+	return 0;
+}
