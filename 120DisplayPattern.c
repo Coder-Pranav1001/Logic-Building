87808208afd8e95b6ad8678e37fc09ab687119be
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-08-29 22:37:38 +0530

    Display Pattern
    Pattern :- 
    	Input = 
    	Row = 6
    	Column = 6
    	Output = 
    	$	$	$	$	$	$
    	$	$				$
    	$		$			$
    	$			$		$
    	$				$	$
    	$	$	$	$	$	$

---
+++ 120DisplayPattern.c
@@ -0,0 +1,57 @@
+/*
+	Description :- Display Pattern
+	Author :- Pranav R Sonawane
+	Pattern :- 
+	Input = 
+	Row = 6
+	Column = 6
+	Output = 
+	$	$	$	$	$	$
+	$	$				$
+	$		$			$
+	$			$		$
+	$				$	$
+	$	$	$	$	$	$
+*/
+#include<stdio.h>
+void Display(int iRow, int iCol)
+{
+	int i = 0;
+	int j = 0;
+	
+	if(iRow != iCol)
+	{
+		printf("Row number and column numbers are different\n");
+		return;
+	}
+	for(i = 1; i <= iRow; i++)
+	{
+		for(j = 1; j <= iCol; j++)
+		{
+			if((i == j) || (i == 1) || (i == iRow) || (j == 1) || (j == iCol))
+			{
+				printf("$\t");
+			}
+			else
+			{
+				printf(" \t");
+			}
+		} 
+		printf("\n");
+	}
+}
+ 
+int main()
+{
+	int iValue1 = 0;
+	int iValue2 = 0;
+	
+	printf("Enter The Value of Rows : \n");
+	scanf("%d",&iValue1);
+	
+	printf("Enter The Value of Columns : \n");
+	scanf("%d",&iValue2);
+	
+	Display(iValue1,iValue2);
+	return 0;
+}
