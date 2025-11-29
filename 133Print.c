cb876fb78eb1141b79f671c58ddf15e333b49aac
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-11-29 23:07:56 +0530

    Print Your Name

---
+++ 133Print.c
@@ -0,0 +1,18 @@
+/*
+	Description :- Print Your Name
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+
+int main()
+{
+	char Arr[50];
+	
+	printf("Please enter your full name : \n");
+	//scanf("%s",Arr);
+	scanf("%[^'\n']s",Arr);
+	
+	printf("Your name is : %s\n",Arr);
+	
+	return 0;
+}
