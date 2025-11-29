35b386f7fcd70a909e7599c7394d20da0ea7d734
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-11-29 23:11:57 +0530

    Calculate Character in String

---
+++ 137Length.c
@@ -0,0 +1,32 @@
+/*
+	Description :- Calculate Character in String
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+
+int strlenX(char str[])
+{
+	int iCnt = 0;
+	int i = 0;
+	
+	for(i = 0; str[i] != '\0'; i++)
+	{
+		iCnt++; 
+	}	
+	return iCnt;
+}
+
+int main()
+{
+	char Arr[20];
+	int iRet = 0;
+	
+	printf("Please enter your full name : \n");
+	scanf("%[^'\n']s",Arr);
+	
+	iRet = strlenX(Arr);
+	
+	printf("Number of characters are : %d\n",iRet);
+	
+	return 0;
+}
