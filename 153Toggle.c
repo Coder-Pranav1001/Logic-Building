b6bb848ebfb1a0831d6d364b281f03b22d46c3ef
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-12-01 23:57:28 +0530

    Convert String Lower to Upper

---
+++ 153Toggle.c
@@ -0,0 +1,35 @@
+/*
+	Description :- Convert String Lower to Upper
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+
+void strToggleX(char *str)
+{
+	while(*str != '\0')
+	{
+		if((*str >= 'a') && (*str <= 'z'))
+		{
+			*str = *str - 32;
+		}
+		else if((*str >= 'A') && (*str <= 'Z'))
+		{
+			*str = *str + 32;
+		}
+		str++;
+	}
+}
+ 
+int main()
+{
+	char Arr[20];
+	
+	printf("Please Enter String : \n");
+	scanf("%[^'\n']s",Arr);
+	
+	strToggleX(Arr);
+	
+	printf("String After Conversion : %s\n",Arr);
+	
+	return 0;
+}
