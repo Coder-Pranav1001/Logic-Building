96679aafec0acb2d42f98be508c74bdcb567e5c6
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-12-01 23:32:51 +0530

    Check weather the character is present or not

---
+++ 145Check.c
@@ -0,0 +1,48 @@
+/*
+	Description :- Check weather the character is present or not
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+#include<stdbool.h>
+
+bool Check(char *str, char ch)
+{
+	bool bFlag = false;
+	
+	while(*str != '\0')
+	{
+		if(*str == ch) 
+ 		{
+			bFlag = true;
+			break;
+		}
+		str++;
+	}
+	return bFlag;
+}
+
+int main()
+{
+	char Arr[20];
+	char cValue = '\0';
+	bool bRet = false;
+	
+	printf("please enter string : \n");
+	scanf("%[^'\n']s",Arr);
+	
+	printf("please Enter the character : \n");
+	scanf(" %c",&cValue);
+	
+	bRet = Check(Arr,cValue);
+	
+	if(bRet == true)
+	{
+		printf("Character is present in the string\n");
+	}
+	else
+	{
+		printf("Character is not present in the string\n");
+	}
+	
+	return 0;
+}
