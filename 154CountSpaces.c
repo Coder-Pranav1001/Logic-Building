ba15ce313e1beadc37419d383b43755bdee2354e
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-12-01 23:58:12 +0530

    Count white spaces in string

---
+++ 154CountSpaces.c
@@ -0,0 +1,35 @@
+/*
+	Description :- Count white spaces in string
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+
+int CountSpace(char *str)
+{
+	int iCnt = 0;
+	
+	while(*str != '\0')
+	{
+		if(*str == ' ')
+		{
+			iCnt++;
+		}
+		str++;
+	}
+	return iCnt;
+}
+  
+int main()
+{
+	char Arr[20];
+	int iRet = 0;
+	
+	printf("Please Enter String : \n");
+	scanf("%[^'\n']s",Arr);
+	
+	iRet = CountSpace(Arr);
+	
+	printf("Number of White Spaces are : %d\n",iRet);
+	
+	return 0;
+}
