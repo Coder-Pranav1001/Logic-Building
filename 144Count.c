88dac053053d94251f5df201acee96b6799423c3
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-12-01 23:32:24 +0530

    Calculate friquency of character in string

---
+++ 144Count.c
@@ -0,0 +1,37 @@
+/*
+	Description :- Calculate friquency of character in string
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+
+int CountFrequency(char *str, char ch)
+{
+	int iCnt = 0;
+	while(*str != '\0')
+	{
+		if(*str == ch) 
+		{
+			iCnt++;
+		}
+		str++;
+	}
+	return iCnt;
+}
+
+int main()
+{
+	char Arr[20];
+	char cValue = '\0';
+	int iRet = 0;
+	
+	printf("please enter string : \n");
+	scanf("%[^'\n']s",Arr);
+	
+	printf("please Enter the character : \n");
+	scanf(" %c",&cValue);
+	
+	iRet = CountFrequency(Arr,cValue);
+	printf("Frequecy of letter : %d",iRet);
+	
+	return 0;
+}
