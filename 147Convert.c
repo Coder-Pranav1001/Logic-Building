f783731edcc1602c3163d32d13903d4fa922a697
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-12-01 23:34:01 +0530

    Convert Character Small Case to Upper Case

---
+++ 147Convert.c
@@ -0,0 +1,24 @@
+/*
+	Description :- Convert Character Small Case to Upper Case
+	Author :- Pranav R Sonawane
+*/
+ #include<stdio.h>
+ 
+ char ToUpperX(char ch)
+ {
+	return ch - 32;
+ }
+  
+ int main()
+ {
+	char cValue = '0';
+	char cRet = '\0';
+	
+	printf("Enter the character : \n");
+	scanf("%c",&cValue);
+	
+	cRet = ToUpperX(cValue);
+	printf("Character in the upppar case is : %c\n",cRet);
+	
+	return 0;
+ }
