6ecb6a5260329e9a8c42a10503f18a0980c56de9
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-01-02 23:01:15 +0530

    Print Jay Ganesh on Console

---
+++ 185Print.cpp
@@ -0,0 +1,13 @@
+/*
+	Description :- Print Jay Ganesh on Console
+	Author :- Pranav R Sonawane
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Jay Ganesh..."<<"\n";
+	
+	return 0;
+}
