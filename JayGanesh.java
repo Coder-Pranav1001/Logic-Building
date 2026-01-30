bbaba9d6a74bb1ad04d63029f1ae3fd4aec2d91c
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-01-30 23:39:23 +0530

    Print Jay Ganesh

---
+++ JayGanesh.java
@@ -0,0 +1,14 @@
+/*
+	Description :- Print Jay Ganesh ...
+	Author :- Pranav R Sonawane
+*/
+
+import java.lang.*;
+
+class JayGanesh
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Jay Ganesh...");
+	}
+}
