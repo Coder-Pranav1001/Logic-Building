de32becc03fcdc827960cd4c77879f69006af151
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-01-30 23:40:37 +0530

    Addition of 2 Integer

---
+++ Addition3.java
@@ -0,0 +1,33 @@
+/*
+	Description :- Addition of 2 Integer
+	Author :- Pranav R Sonawane
+*/
+
+import java.util.*;
+
+class Addition3
+{
+	static int Addition(int i, int j)
+	{
+		int Sum = 0;
+		Sum = i + j;
+		return Sum;
+	}
+	
+	public static void main(String arg[])
+	{
+		Scanner sobj = new Scanner(System.in);
+		
+		int iNo1 = 0, iNo2 = 0, iAns = 0;
+		
+		System.out.println("Enter the First Number");
+		iNo1 = sobj.nextInt();
+		
+		System.out.println("Enter the Second Number");
+		iNo2 = sobj.nextInt();
+		
+		iAns = Addition(iNo1,iNo2);
+		
+		System.out.println("Addition is : "+iAns);
+	}
+}
