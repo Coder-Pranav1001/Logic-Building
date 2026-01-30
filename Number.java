8edbbf86eda4e3c7c03d5a3ce8b61bad53883c4a
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-01-30 23:41:40 +0530

    Templete for problems on number

---
+++ Number.java
@@ -0,0 +1,25 @@
+/*
+	Description :- Templete for problems on number
+	Author :- Pranav R Sonawane
+*/
+import java.util.*;	
+
+class Numbers
+{
+	// Method
+}
+
+class Number
+{
+	public static void main(String str[])
+	{
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("Enter the number : ");
+		int iNo = sobj.nextInt();
+		
+		Numbers nobj = new Numbers();
+		
+		nobj.____();
+	}
+}
