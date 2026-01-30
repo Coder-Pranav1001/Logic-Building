27935ec5032eb4212b7c71a46c580b3681389f9c
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-01-30 23:39:59 +0530

    Addition of  two Integer

---
+++ Addition1.java
@@ -0,0 +1,16 @@
+/*
+	Description :- Addition of 2 Integer
+	Author :- Pranav R Sonawane
+*/
+
+class Addition1
+{
+	public static void main(String arg[])
+	{
+		int iNo1 = 10, iNo2 = 11, iAns = 0;
+		
+		iAns = iNo1 + iNo2;
+		
+		System.out.println("Addition is : "+iAns); 
+	}
+}
