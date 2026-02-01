e65e12a7dd2436aa93e7a1e51e088d685d9df2ff
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-01 23:33:56 +0530

    How to Create Array

---
+++ Templete.java
@@ -0,0 +1,23 @@
+/*
+	Description :- How to Create Array
+	Author :- Pranav R Sonawane
+*/
+
+class ArrayX
+{
+	public int Arr[];
+	
+	public ArrayX(int iSize)
+	{
+		Arr = new int[iSize];
+		
+	}
+}
+
+class Templete
+{
+	public static void main(String arg[])
+	{
+		ArrayX obj = new ArrayX(5);
+	}
+}
