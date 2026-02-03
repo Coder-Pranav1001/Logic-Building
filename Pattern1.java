6e01dea921009e7fb20f1565be79fd857b5955ca
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-04 00:11:35 +0530

    Templete for Pattern

---
+++ Pattern1.java
@@ -0,0 +1,24 @@
+/*
+	Description :- Templete for Pattern
+	Author :- Pranav R Sonawane
+*/
+import java.util.*;
+
+class Pattern
+{
+	public void Display()
+	{
+		//Logic
+	}
+}
+ 
+class Pattern1
+{
+	public static void main(String a[])
+	{
+		Pattern pobj = new Pattern();
+		Scanner sobj = new Scanner(System.in);
+		
+		pobj.Display();
+	}
+}
