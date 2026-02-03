b677cca065e2034cd4e2591f0ec01788b7619b12
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-04 00:12:15 +0530

    Pattern (Accept Input From User and Display Pattern)
            Rows = 4
    	Columns = 4
    	
    	Pattern :
    	
    	*	*	*	*
    	*	*	*	*
    	*	*	*	*
    	*	*	*	*

---
+++ Pattern2.java
@@ -0,0 +1,51 @@
+/*
+	Description :- Pattern (Accept Input From User and Display Pattern)
+	Author :- Pranav R Sonawane
+*/
+/*
+	Rows = 4
+	Columns = 4
+	
+	Pattern :
+	
+	*	*	*	*
+	*	*	*	*
+	*	*	*	*
+	*	*	*	*
+*/
+
+import java.util.*;
+
+class Pattern
+{
+	public void Display(int iRow, int iCol)
+	{
+		int i = 0, j = 0;
+		
+		for(i = 1; i <= iRow; i++)
+		{
+			for(j = 1; j <= iCol; j++)
+			{
+				
+			}
+			System.out.println();
+		}
+	}
+}
+ 
+class Pattern2
+{
+	public static void main(String a[])
+	{
+		Pattern pobj = new Pattern();
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("Enter number of rows : ");
+		int i = sobj.nextInt();
+		
+		System.out.println("Enter number of columns : ");
+		int j = sobj.nextInt();
+		
+		pobj.Display(i,j);
+	}
+}
