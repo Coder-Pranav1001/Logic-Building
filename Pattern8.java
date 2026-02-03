1210cfd7cd79a05d561d2ce4c64944a5d65669a4
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-04 00:17:16 +0530

    Pattern (Accept Input From User and Display Pattern)
            Input : Hello
    	
    	Pattern :
    	
    	H	e	l	l	o
    	H	e	l	l	o
    	H	e	l	l	o
    	H	e	l	l	o
    	H	e	l	l	o

---
+++ Pattern8.java
@@ -0,0 +1,49 @@
+/*
+	Description :- Pattern (Accept Input From User and Display Pattern)
+	Author :- Pranav R Sonawane
+*/
+/*
+	Input : Hello
+	
+	Pattern :
+	
+	H	e	l	l	o
+	H	e	l	l	o
+	H	e	l	l	o
+	H	e	l	l	o
+	H	e	l	l	o
+*/
+
+import java.util.*;
+
+class Pattern
+{
+	public void Display(String str)
+	{
+		char Arr[] = str.toCharArray();
+		int i = 0, j = 0;
+		
+		for(i = 0; i < Arr.length; i++)
+		{
+			for(j = 0; j < Arr.length; j++)
+			{
+				System.out.print(Arr[j]+"\t");
+			}
+			System.out.println();
+		}
+	}
+}
+ 
+class Pattern8
+{
+	public static void main(String a[])
+	{
+		Pattern pobj = new Pattern();
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("Enter string : ");
+		String s = sobj.nextLine(); 
+		
+		pobj.Display(s);
+	}
+}
