f293afb813285c72a5076987ccde8b7a567e61c4
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-05 17:04:20 +0530

    Swapping two numbers using Generic

---
+++ 219Swaping.cpp
@@ -0,0 +1,37 @@
+/*
+	Description :- Swapping two numbers using Generic
+	Author :- Pranav R Sonawane
+*/
+
+#include<iostream>
+using namespace std; 
+
+// call by reference
+template<class T>
+void Swap(T &x, T &y)
+{
+	T temp;
+	
+	temp = x;
+	x = y;
+	y = temp;
+}
+
+int main()
+{
+	int a = 11, b = 10;
+	cout<<"Value of a "<<a<<"\n";
+	cout<<"Value of b "<<b<<"\n";
+	Swap(a,b);
+	cout<<"Value of a "<<a<<"\n";
+	cout<<"Value of b "<<b<<"\n";
+	
+	double p = 11.9, q = 10.7;
+	cout<<"Value of a "<<p<<"\n";
+	cout<<"Value of b "<<q<<"\n";
+	Swap(p,q);
+	cout<<"Value of a "<<p<<"\n";
+	cout<<"Value of b "<<q<<"\n"; 
+	
+	return 0;
+}
