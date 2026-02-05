da9cdaf1174cae12f5614da8910d42c4dbdf1ae2
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-05 17:04:03 +0530

    Swapping two numbers using Specific

---
+++ 218Swaping.cpp
@@ -0,0 +1,45 @@
+/*
+	Description :- Swapping two numbers using Specific
+	Author :- Pranav R Sonawane
+*/
+
+#include<iostream>
+using namespace std; 
+
+// call by reference
+void Swapi(int &x, int &y)
+{
+	int temp;
+	
+	temp = x;
+	x = y;
+	y = temp;
+}
+
+void Swapd(double &x, double &y)
+{
+	double temp;
+	
+	temp = x;
+	x = y;
+	y = temp;
+}
+
+int main()
+{
+	int a = 11, b = 10;
+	cout<<"Value of a "<<a<<"\n";
+	cout<<"Value of b "<<b<<"\n";
+	Swapi(a,b);
+	cout<<"Value of a "<<a<<"\n";
+	cout<<"Value of b "<<b<<"\n";
+	
+	double p = 11.9, q = 10.7;
+	cout<<"Value of a "<<p<<"\n";
+	cout<<"Value of b "<<q<<"\n";
+	Swapd(p,q);
+	cout<<"Value of a "<<p<<"\n";
+	cout<<"Value of b "<<q<<"\n"; 
+	
+	return 0;
+}
