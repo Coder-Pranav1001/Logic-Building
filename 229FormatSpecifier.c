f158d7bb51b8753e8f20d054fb0a9b9ddfd97185
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-06 23:15:30 +0530

    Value Format Specifier

---
+++ 229FormatSpecifier.c
@@ -0,0 +1,17 @@
+/*
+	Description :- Value FormatSpecifier
+	Author :- Pranav R Sonawane
+*/ 
+
+#include<stdio.h>
+
+int main()
+{
+	unsigned int Value = 11;
+	
+	printf("Value of decimal format : %d\n",Value);
+	printf("Value of octal format : %o\n",Value);
+	printf("Value of hexadecimal format : %x\n",Value);	
+	
+	return 0;
+}
