8e6cb1581175ec1dc148bd7bd9824ef157d6e8f0
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-09 23:41:20 +0530

    ( Masking ) Accept no from user and check weather the 10th Bit of that number is ON or OFF

---
+++ 234Bitwise.c
@@ -0,0 +1,47 @@
+/*
+	Description :- ( Masking ) Accept no from user and check weather the 10th Bit of that number is ON or OFF
+	Author :- Pranav R Sonawane
+*/ 
+#include<stdio.h>
+#include<stdbool.h>
+
+typedef unsigned int UINT;
+
+bool CheckBit(UINT No)
+{
+	UINT iMask = 512;
+	UINT Result = 0;
+	
+	Result = No & iMask;
+	
+	if(Result == iMask)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int main()
+{
+	UINT Value = 0;
+	bool bRet = false;
+	
+	printf("Enter the Number : \n");
+	scanf("%d",&Value);
+	
+	bRet = CheckBit(Value);
+	
+	if(bRet == true)
+	{
+		printf("10th Bit is ON\n");
+	}
+	else
+	{
+		printf("10th Bit is OFF\n");
+	}
+	
+	return 0;
+}
