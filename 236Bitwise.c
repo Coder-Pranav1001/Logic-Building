91f0521b811f66842a4fd709d1f9aab902c6a1c0
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-09 23:41:54 +0530

    ( Masking ) Accept no from user and check weather the 12th Bit of that number is ON or OFF

---
+++ 236Bitwise.c
@@ -0,0 +1,54 @@
+/*
+	Description :- ( Masking ) Accept no from user and check weather the 12th Bit of that number is ON or OFF
+	Author :- Pranav R Sonawane
+*/ 
+#include<stdio.h>
+#include<stdbool.h>
+
+typedef unsigned int UINT;
+
+//  12th Bit
+// 	0000 	0000 	0000 	0000 	0000 	0000 	0000 	0000
+// 	0000 	0000 	0000 	0000 	0000 	1000 	0000 	0000
+//    0		  0		  0	      0       0       8        0       0
+//	00000800
+//  0X00000800
+
+bool CheckBit(UINT No)
+{
+	UINT iMask = 0X00000800;
+	UINT Result = 0;
+	
+	Result = No & iMask;
+	
+	if(Result == iMask)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int main()
+{
+	UINT Value = 0;
+	bool bRet = false;
+	
+	printf("Enter the Number : \n");
+	scanf("%d",&Value);
+	
+	bRet = CheckBit(Value);
+	
+	if(bRet == true)
+	{
+		printf("12th Bit is ON\n");
+	}
+	else
+	{
+		printf("12th Bit is OFF\n");
+	}
+	
+	return 0;
+}
