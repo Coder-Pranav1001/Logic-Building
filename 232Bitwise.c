265a3f925f7ffb89ca7b949fc9ee1d51ae5fa21c
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-09 23:40:20 +0530

    Count Bits which are on without using if part

---
+++ 232Bitwise.c
@@ -0,0 +1,36 @@
+/*
+	Description :- Count Bits which are on without using if part 
+	Author :- Pranav R Sonawane
+*/ 
+
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+int CountOnBits(UINT no)
+{
+	int iCnt = 0;
+	int Digit = 0;
+	
+	while(no != 0)
+	{
+		Digit = no % 2;
+		iCnt = iCnt + Digit;
+		no = no / 2;
+	}
+	return iCnt;
+}
+
+int main()
+{
+	UINT Value = 0;
+	int Ret = 0;
+	
+	printf("Enter the Number : \n");
+	scanf("%d",&Value);
+	
+	Ret = CountOnBits(Value);
+	printf("Number of bits which are ON are : %d\n",Ret);
+	
+	return 0;
+}
