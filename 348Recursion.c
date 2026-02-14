4c3458bcdeb8022d9a4351980220023fb4d0d0e2
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-14 19:05:34 +0530

    Display Pattern using Recursion

---
+++ 348Recursion.c
@@ -0,0 +1,36 @@
+/*
+	Description :- Display Pattern using Recursion
+	Author :- Pranav R Sonawane
+*/ 
+
+#include<stdio.h>
+
+void DisplayI()
+{
+	int iCnt = 1; // auto
+	
+	while(iCnt <= 4)
+	{
+		printf("*\t");
+		iCnt++;
+	}
+}
+
+void DisplayR()
+{
+	static int iCnt = 1;
+	
+	if(iCnt <= 4)
+	{
+		printf("*\t");
+		iCnt++;
+		DisplayR(); // Recursive Call
+	}
+}
+
+int main()
+{
+	DisplayR();
+	
+	return 0;
+}
