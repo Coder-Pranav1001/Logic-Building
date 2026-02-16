61b378d1096bc6e3212db8d150c9546957f7b2aa
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-16 19:21:08 +0530

    Accept String from user and display count of small characters of the string using recursion

---
+++ 367Recursion.c
@@ -0,0 +1,36 @@
+/*
+	Description :- Accept String from user and display count of small characters of the string using recursion
+	Author :- Pranav R Sonawane
+*/ 
+#include<stdio.h>
+
+int CountCapitalR(char *str)
+{
+	static int iCnt = 0;
+	
+	if(*str != '\0')
+	{
+		if(*str >= 'a' && *str <= 'z')
+		{
+			iCnt++;
+		}
+		str++;
+		CountCapitalR(str);
+	}
+	return iCnt;
+}
+
+int main()
+{
+	char Arr[20];
+	int iRet = 0;
+	
+	printf("Enter the string\n");
+	scanf("%[^'\n's]",Arr);
+	
+	iRet = CountCapitalR(Arr);
+	
+	printf("Chapital Charater is : %d\n",iRet);
+	
+	return 0;
+}
