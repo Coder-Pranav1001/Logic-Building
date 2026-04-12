bc518f7fec764ac75881f90430d4a3198c0c61fb
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-04-13 00:13:14 +0530

    Hash Map (frequency occurs of Word)

---
+++ HashMap4.java
@@ -0,0 +1,37 @@
+/*
+	Description :- Hash Map (frequency occurs of Word)
+	Author :- Pranav R Sonawane
+*/
+import java.util.*;
+
+class HashMap4
+{
+	public static void main(String a[])
+	{
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("Enter String : ");
+		String str = sobj.nextLine();
+		
+		String Arr[] = str.split(" ");
+		
+		//		  key     value
+		HashMap <String, Integer> hobj = new HashMap(); // object creation of HashMap
+		int Frequency = 0;
+		
+		for(String ch : Arr)
+		{
+			if(hobj.containsKey(ch))
+			{
+				Frequency = hobj.get(ch);
+				hobj.put(ch,Frequency+1);
+			}
+			else 
+			{
+				hobj.put(ch,1);
+			}
+		}
+		
+		System.out.println(hobj);
+	}
+}
